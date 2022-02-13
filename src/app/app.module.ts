3b7213e85f7c34507af3afacff21bab6a8e82d10
Irving González <irgonzalez@fmf.mx>
2022-02-12 21:24:34 -0600

    Configuration firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { FooterComponent } from './share/footer/footer.component';
 import { NavbarComponent } from './share/navbar/navbar.component';
 import { SidebarComponent } from './share/sidebar/sidebar.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -31,7 +35,10 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'redux-egreso-app',
+    appId: '1:22479059412:web:fdd1c5a43e4b230c2bbe41',
+    storageBucket: 'redux-egreso-app.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyClkAj-O_Uke2FVZy9-rDfowOAGOP9kzQg',
+    authDomain: 'redux-egreso-app.firebaseapp.com',
+    messagingSenderId: '22479059412',
+  },
   production: true
 };
